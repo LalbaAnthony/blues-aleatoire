114a32fb73dfebf99e87ca0f8a5dd0c9e0064264
LalbaAnthony <anthony.lalba@limayrac.fr>
2023-12-06 21:01:01 +0100

    Push

--- scales.js
+++ scales.js
@@ -1,5 +1,14 @@
 const scales = {
-    ApentatonicMinor: [
+    //   0  1  2  3  4  5  6  7  8  9  10 11 12
+    GMinorPentatonic: [
+        [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+    ],
+    AMinorPentatonic: [
         [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
         [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
@@ -7,14 +16,26 @@ const scales = {
         [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0],
         [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
     ],
+    BMinorPentatonic: [
+        [1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0],
+        [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+        [1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+    ],
 };
+
+function getScale(rootTone = 'A', scaleQuality = 'minor', scale = 'pentatonic') {
+    rootTone = rootTone.toUpperCase().trim();
+    scaleQuality = (scaleQuality.charAt(0).toUpperCase() + scaleQuality.slice(1)).trim();
+    scale = (scale.charAt(0).toUpperCase() + scale.slice(1)).trim();
+    const fullScaleName = `${rootTone}${scaleQuality}${scale}`;
+    return scales[fullScaleName];
+}
 
 function getScales() {
     return scales;
-}
-
-function getScale(scaleName) {
-    return scales[scaleName];
 }
 
 function getScalesNames() {
